3278ecea7ab35ce38cad8870b93ced5f654f309c
Bohdan <telanim@outlook.com>
2025-04-29 18:41:36 +0300

    TodoView object

--- js/controllers/todoController.js
+++ js/controllers/todoController.js
@@ -1,2 +1,4 @@
 import { TodoItem } from "../models/todoItem";
 import { Storage } from "../models/storage";
+import { TodoView } from "../views/todoView";
+
---
+++ js/views/todoView.js
@@ -0,0 +1,34 @@
+export class TodoView {
+  constructor() {
+    this.taskContainer = document.getElementById('task-container');
+    this.taskInput = document.getElementById('task-input');
+    this.addTaskBtn = document.getElementById('add-task-btn');
+  }
+
+  renderList(list) {
+    this.taskContainer.innerHTML = "";
+
+    if(list.length === 0) {
+      this.taskContainer.innerHTML = 'No tasks found';
+      return;
+    }
+
+    list.forEach(item => {
+      this.renderItem(item);
+    });
+  }
+  renderItem(item) {
+    const htmlItem = document.createElement('li');
+    htmlItem.classList.add('doto-item');
+    htmlItem.dataset.id = item.id;
+
+    htmlItem.innerHTML = `
+      <div class="todo-item-content">
+        <input type="checkbox" class="toggle" ${item.completed ? 'checked' : ''}>
+        <span class="todo-text">${item.title}</span>
+        <input type="text" class="edit-input" value="${item.title}">
+      </div>
+    `;
+    this.taskContainer.appendChild(htmlItem);
+  }
+}
